a5dbb93b4e05579e252546b94f2215f820169b49
ityeri <ityeri9@gmail.com>
2026-04-28 15:41:48 +0900

    add: VideoComponent

--- src/yspy/page_parsing/__init__.py
+++ src/yspy/page_parsing/__init__.py
@@ -3,9 +3,11 @@ TODO
 """
 
 from .channel_component import ChannelComponent
+from .video_component import VideoComponent
 from .thumbnail_component import ThumbnailComponent
 
 __all__ = [
     'ChannelComponent',
+    'VideoComponent',
     'ThumbnailComponent'
 ]
---
+++ src/yspy/page_parsing/video_component.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+from .thumbnail_component import ThumbnailComponent
+from yspy.utils import get_by_path
+
+
+@dataclass(frozen=True)
+class VideoComponent:
+    id: str
+    title: str
+    url: str
+    thumbnails: list[ThumbnailComponent]
+    is_shorts: bool
+    published_time_text: str
+    length_text: str
+    view_count_text: str
+    channel_url: str
+
+    @staticmethod
+    def from_json(raw_data: dict[str, dict]) -> VideoComponent:
+        try:
+            inner_data = raw_data['videoRenderer']
+        except KeyError:
+            raise ValueError('Given json data is not a channel renderer data')
+
+        return VideoComponent(
+            id=inner_data['videoId'],
+            title=get_by_path(inner_data, 'title runs', 0, 'text'),
+            url='https://youtube.com'
+                + get_by_path(inner_data, 'navigationEndpoint commandMetadata webCommandMetadata url'),
+            thumbnails=[
+                ThumbnailComponent(
+                    url='https://' + raw_thumbnail_data['url'].strip('/'),
+                    width=int(raw_thumbnail_data['width']),
+                    height=int(raw_thumbnail_data['height'])
+                ) for raw_thumbnail_data in get_by_path(inner_data, 'thumbnail thumbnails')
+            ],
+            is_shorts='reelWatchEndpoint' in inner_data['navigationEndpoint'],
+            published_time_text=get_by_path(inner_data, 'publishedTimeText simpleText'),
+            length_text=get_by_path(inner_data, 'lengthText simpleText'),
+            view_count_text = get_by_path(inner_data, 'viewCountText simpleText'),
+            channel_url='https://youtube.com' + get_by_path(
+                inner_data,
+                'ownerText runs', 0, 'navigationEndpoint commandMetadata webCommandMetadata url'
+            )
+        )
