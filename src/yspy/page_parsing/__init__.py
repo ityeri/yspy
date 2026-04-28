7006456e68e4ddaf28fb9a73a1db78b403aaa8f3
ityeri <ityeri9@gmail.com>
2026-04-28 17:59:04 +0900

    add: Missing classes to page_parsing.__init__

--- src/yspy/page_parsing/__init__.py
+++ src/yspy/page_parsing/__init__.py
@@ -2,12 +2,16 @@
 TODO
 """
 
-from .channel_component import ChannelComponent
+from .search_result_page import SearchResultPage
+from .search_result_component import SearchResultComponent
 from .video_component import VideoComponent
+from .channel_component import ChannelComponent
 from .thumbnail_component import ThumbnailComponent
 
 __all__ = [
-    'ChannelComponent',
+    'SearchResultPage',
+    'SearchResultComponent',
     'VideoComponent',
-    'ThumbnailComponent'
+    'ChannelComponent',
+    'ThumbnailComponent',
 ]
