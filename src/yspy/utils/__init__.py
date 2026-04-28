5c3b8d30ee91cf547817b4e2102946b71cc274f6
ityeri <ityeri9@gmail.com>
2026-04-28 14:10:01 +0900

    edit: Made a get_by_path func able to processing multi-type path

--- src/yspy/utils/__init__.py
+++ src/yspy/utils/__init__.py
@@ -1,10 +1,16 @@
-def get_by_path(data: str | dict | list, path: str) -> str | dict | list:
-    path_parts = path.split()
+def get_by_path(data: str | dict | list, *path: str | int) -> str | dict | list:
+    path_parts = list()
+
+    for path_chunk in path:
+        if isinstance(path_chunk, str):
+            path_parts.extend(path_chunk.split())
+        elif isinstance(path_chunk, int):
+            path_parts.append(path_chunk)
 
     if len(path_parts) == 0:
         return data
     else:
-        return get_by_path(data[path_parts[0]], ' '.join(path_parts[1:]))
+        return get_by_path(data[path_parts[0]], *path_parts[:1])
 
 
 __all__ = [
