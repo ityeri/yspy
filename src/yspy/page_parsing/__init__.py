46c9e0c2a6022820a2b83cd3bc78844d30c70c53
ityeri <ityeri9@gmail.com>
2026-04-29 20:53:20 +0900

    add: PlaylistVideoComponent class and test code for it

--- src/yspy/page_parsing/__init__.py
+++ src/yspy/page_parsing/__init__.py
@@ -2,21 +2,25 @@
 TODO
 """
 
-from .search_result_page import SearchResultPage
-from .search_result_component import SearchResultComponent
-from .video_component import VideoComponent
 from .channel_component import ChannelComponent
-from .image_component import ImageComponent
-from .channel_page import ChannelPage
 from .channel_detail_page import ChannelDetailPage
+from .channel_page import ChannelPage
+from .image_component import ImageComponent
+from .playlist_video_component import PlaylistVideoComponent
+from .search_result_component import SearchResultComponent
+from .search_result_page import SearchResultPage
+from .video_component import VideoComponent
 
 __all__ = [
+    'ImageComponent',
+
     'SearchResultPage',
     'SearchResultComponent',
     'VideoComponent',
     'ChannelComponent',
-    'ImageComponent',
 
     'ChannelPage',
-    'ChannelDetailPage'
+    'ChannelDetailPage',
+
+    'PlaylistVideoComponent'
 ]
---
+++ src/yspy/page_parsing/playlist_video_component.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from .image_component import ImageComponent
+from ..utils import get_by_path
+
+
+@dataclass
+class PlaylistVideoComponent:
+    id: str
+    title: str
+    url: str
+    thumbnails: list[ImageComponent]
+    index: int
+    owner_text: str
+    owner_url: str
+    length_text: str
+    length_seconds: int
+    is_playable: bool
+
+    @staticmethod
+    def from_json(raw_data: dict[str, dict]) -> PlaylistVideoComponent:
+        try:
+            inner_data = raw_data['playlistVideoRenderer']
+        except KeyError:
+            raise ValueError('Given data is not a playlist video renderer data')
+
+        return PlaylistVideoComponent(
+            id=inner_data['videoId'],
+            title=get_by_path(inner_data, 'title runs', 0, 'text'),
+            url='https://youtube.com'
+                + get_by_path(inner_data, 'navigationEndpoint commandMetadata webCommandMetadata url'),
+            thumbnails=[
+                ImageComponent.from_json(
+                    raw_thumbnail_data
+                )
+                for raw_thumbnail_data in get_by_path(inner_data, 'thumbnail thumbnails')
+            ],
+            index=int(get_by_path(inner_data, 'index simpleText')),
+            owner_text=get_by_path(inner_data, 'shortBylineText runs', 0, 'text'),
+            owner_url='https://youtube.com' + get_by_path(
+                inner_data,
+                'shortBylineText runs', 0, 'navigationEndpoint commandMetadata webCommandMetadata url'
+            ),
+            length_text=get_by_path(inner_data, 'lengthText simpleText'),
+            length_seconds=int(get_by_path(inner_data, 'lengthSeconds')),
+            is_playable=inner_data['isPlayable']
+        )
