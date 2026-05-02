3f7754b096eb98c34c3a897a19cdecaca2e05dfe
ityeri <ityeri9@gmail.com>
2026-05-02 13:17:27 +0900

    add: Test code of PlaylistPage class

---
+++ tests/test_playlist_page.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+from yspy.page_parsing.playlist_page import PlaylistPage
+
+with open(Path(__file__).parent / 'test_datas/full_page/test_playlist_page.json', 'r') as f:
+    raw_data = json.load(f)
+
+
+playlist_page = PlaylistPage.from_json(raw_data)
+
+breakpoint()
