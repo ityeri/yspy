49fe94f3649e306e39084f46d4435e071100bdcf
ityeri <ityeri9@gmail.com>
2026-05-02 11:21:49 +0900

    add: PlaylistPage

---
+++ src/yspy/page_parsing/playlist_page.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from .playlist_video_component import PlaylistVideoComponent
+from .image_component import ImageComponent
+from yspy.utils import get_by_path
+
+
+@dataclass
+class PlaylistPage:
+    id: str
+    title: str
+    url: str
+    thumbnails: list[ImageComponent]
+    owner_text: str
+    owner_url: str
+    view_count_text: str
+    videos: list[PlaylistVideoComponent]
+
+    @staticmethod
+    def from_json(raw_data: dict[str, dict]) -> PlaylistPage:
+        try:
+            sidebar_items: list[dict[str, dict]] = \
+                get_by_path(raw_data, 'sidebar, playlistSidebarRenderer items')
+            primary_info_renderer = sidebar_items[0]['playlistSidebarPrimaryInfoRenderer']
+            secondary_info_renderer = sidebar_items[1]['playlistSidebarSecondaryInfoRenderer']
+
+            video_items: list[dict[str, dict]] = get_by_path(
+                raw_data,
+                'contents twoColumnBrowseResultsRenderer tabs', 0,
+                'tabRenderer content sectionListRenderer contents', 0,
+                'playlistVideoListRenderer contents'
+            )
+        except KeyError:
+            raise ValueError('Given data is not a playlist page data')
+
+        return PlaylistPage(
+            id=get_by_path(
+                primary_info_renderer,
+                'title runs', 0, 'navigationEndpoint watchEndpoint playlistId'
+            ),
+            title=get_by_path(primary_info_renderer, 'title runs', 0, 'text'),
+            url='https://youtube.com' + get_by_path(
+                primary_info_renderer,
+                'title runs', 0, 'navigationEndpoint commandMetadata webCommandMetadata url'
+            ),
+            thumbnails=[
+                ImageComponent.from_json(raw_thumbnail_data)
+                for raw_thumbnail_data in get_by_path(
+                    primary_info_renderer,
+                    'thumbnailRenderer playlistVideoThumbnailRenderer thumbnail thumbnails'
+                )
+            ],
+            owner_text=get_by_path(
+                secondary_info_renderer,
+                'videoOwner videoOwnerRenderer title runs', 0, 'text'
+            ),
+            owner_url='https://youtube.com' + get_by_path(
+                secondary_info_renderer,
+                'videoOwner videoOwnerRenderer title runs', 0,
+                'navigationEndpoint browseEndpoint canonicalBaseUrl'
+            ),
+            view_count_text=get_by_path(primary_info_renderer, 'stats', 1, 'simpleText'),
+            videos=[
+                PlaylistVideoComponent.from_json(raw_video_data)
+                for raw_video_data in video_items
+            ]
+        )
