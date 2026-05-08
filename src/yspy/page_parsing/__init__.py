d5e42cd4234da4d057acbf8395f52ec5df2ac517
ityeri <ityeri9@gmail.com>
2026-05-09 01:02:28 +0900

    add: ContinuationVideoPage class

--- src/yspy/page_parsing/__init__.py
+++ src/yspy/page_parsing/__init__.py
@@ -5,6 +5,7 @@ TODO
 from .channel_component import ChannelComponent
 from .channel_detail_page import ChannelDetailPage
 from .channel_page import ChannelPage
+from .continuation_video_page import ContinuationVideoPage
 from .image_component import ImageComponent
 from .playlist_page import PlaylistPage
 from .playlist_video_component import PlaylistVideoComponent
@@ -27,5 +28,7 @@ __all__ = [
     'PlaylistPage',
     'PlaylistVideoComponent',
 
-    'VideoPage'
+    'VideoPage',
+
+    'ContinuationVideoPage'
 ]
---
+++ src/yspy/page_parsing/continuation_video_page.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from yspy.utils import get_by_path
+
+
+@dataclass
+class ContinuationVideoPage:
+    comment_continuation_token: str
+
+    @staticmethod
+    def from_json(raw_data: dict[str, dict]) -> ContinuationVideoPage:
+        try:
+            return ContinuationVideoPage(
+                comment_continuation_token=get_by_path(
+                    raw_data,
+                    'engagementPanels', 0, 'engagementPanelSectionListRenderer content sectionListRenderer',
+                    'contents', 0, 'itemSectionRenderer',
+                    'contents', 0,
+                    'continuationItemRenderer continuationEndpoint continuationCommand token'
+                )
+            )
+        except KeyError:
+            raise ValueError('The given data is not a video continuation page')
