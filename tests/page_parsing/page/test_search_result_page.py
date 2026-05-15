59513968dccfebc6e06d9de853620421a3f32b78
ityeri <ityeri9@gmail.com>
2026-05-15 11:14:26 -0700

    add: Test code for search result continuation page

--- tests/page_parsing/page/test_search_result_page.py
+++ tests/page_parsing/page/test_search_result_page.py
@@ -4,8 +4,11 @@ from pathlib import Path
 from yspy.page_parsing import SearchResultPage
 
 with open(Path(__file__).parent / 'test_datas/search_result_page.json', 'r', encoding='utf-8') as f:
-    test_first_search_result = json.load(f)
+    search_result_page = json.load(f)
+with open(Path(__file__).parent / 'test_datas/search_result_continuation_page.json', 'r', encoding='utf-8') as f:
+    search_result_continuation_page = json.load(f)
 
-first_page = SearchResultPage.from_json(test_first_search_result, is_continuation_page=False)
+first_page = SearchResultPage.from_json(search_result_page)
+continuation_page = SearchResultPage.from_json(search_result_continuation_page)
 
 breakpoint()
