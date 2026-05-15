ad861aaa4d46687a8eb8b316e87046c5724972f0
ityeri <ityeri9@gmail.com>
2026-05-15 10:49:05 -0700

    add: PlaylistPage continuation test code, data

--- tests/page_parsing/page/test_playlist_page.py
+++ tests/page_parsing/page/test_playlist_page.py
@@ -4,9 +4,11 @@ from pathlib import Path
 from yspy.page_parsing import PlaylistPage
 
 with open(Path(__file__).parent / 'test_datas/playlist_page.json', 'r') as f:
-    raw_data = json.load(f)
+    raw_playlist_page_data = json.load(f)
+with open(Path(__file__).parent / 'test_datas/playlist_continuation_page.json', 'r') as f:
+    raw_continuation_page_data = json.load(f)
 
-
-playlist_page = PlaylistPage.from_json(raw_data)
+playlist_page = PlaylistPage.from_json(raw_playlist_page_data)
+continuation_page = PlaylistPage.from_json(raw_continuation_page_data)
 
 breakpoint()
