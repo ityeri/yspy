4a8f831e840e2fe4c895f8b7e1bd75a08816938f
ityeri <ityeri9@gmail.com>
2026-05-22 18:43:23 +0900

    add: build_request_body util function

--- src/yspy/request/utils.py
+++ src/yspy/request/utils.py
@@ -1,10 +1,17 @@
 from contextlib import asynccontextmanager
-from typing import Optional, AsyncIterator
+from typing import Optional, AsyncIterator, Any
 from urllib.parse import urlencode
 
 import httpx
 from httpx import AsyncClient
 
+from yspy.utils import Language, Region
+
+BASE_CLIENT_DATA = {
+    'clientName': 'WEB',
+    'clientVersion': '2.20210224.06.00',
+    'newVisitorCookie': True,
+}
 
 BASE_REQUEST_BODY = {
     'context': {
@@ -40,6 +47,29 @@ def build_browse_url(parameters: dict[str, str] = None) -> str:
             'https://www.youtube.com/youtubei/v1/browse'
             + '?' + urlencode({'key': BROWSE_KEY} | parameters)
     )
+
+def build_request_body(
+        parameters: dict[str, Any],
+        *,
+        language: Language | None = None,
+        region: Region | None = None
+) -> dict[str, Any]:
+    other_client_data = dict()
+
+    if language is not None:
+        other_client_data['hl'] = language
+    if region is not None:
+        other_client_data['gl'] = region
+
+    return {
+        'context': {
+            'client': BASE_CLIENT_DATA | other_client_data,
+            'user': {
+                'lockedSafetyMode': False,
+            }
+        },
+        **parameters
+    }
 
 @asynccontextmanager
 async def optional_async_client(client: AsyncClient | None = None) -> AsyncIterator[httpx.AsyncClient]:
