2fbc533b0a34b52cf8590cdc5fed932b5c82e14b
ityeri <ityeri9@gmail.com>
2026-05-22 18:45:38 +0900

    edit: Made playlist.py file uses the build_request_body function

--- src/yspy/request/playlist.py
+++ src/yspy/request/playlist.py
@@ -1,15 +1,15 @@
 from httpx import AsyncClient
 
-from .utils import optional_async_client, build_browse_url, BASE_REQUEST_BODY, BASE_HEADERS
+from .utils import optional_async_client, build_browse_url, BASE_HEADERS, build_request_body
 
 
 async def get_first_page(playlist_id: str, client: AsyncClient | None = None) -> dict[str, dict]:
     async with optional_async_client(client) as client:
         response = await client.post(
             url=build_browse_url(),
-            json=BASE_REQUEST_BODY | {
+            json=build_request_body({
                 'browseId': 'VL' + playlist_id if not playlist_id.startswith('VL') else playlist_id
-            },
+            }),
             headers=BASE_HEADERS
         )
 
@@ -19,9 +19,9 @@ async def get_continuation_page(continuation_token: str, client: AsyncClient | N
     async with optional_async_client(client) as client:
         response = await client.post(
             url=build_browse_url(),
-            json=BASE_REQUEST_BODY | {
+            json=build_request_body({
                 'continuation': continuation_token
-            },
+            }),
             headers=BASE_HEADERS
         )
 
