0f9123937d47a430d595b47fdb5bc193ccb2b237
ityeri <ityeri9@gmail.com>
2026-05-23 20:18:26 +0900

    add: NEXT_API_URL to utils.py

--- src/yspy/request/utils.py
+++ src/yspy/request/utils.py
@@ -30,6 +30,7 @@ BASE_HEADERS = {
 BROWSE_API_URL = 'https://www.youtube.com/youtubei/v1/browse'
 SEARCH_API_URL = 'https://www.youtube.com/youtubei/v1/search'
 PLAYER_API_URL = 'https://www.youtube.com/youtubei/v1/player'
+NEXT_API_URL = 'https://www.youtube.com/youtubei/v1/next'
 BROWSE_KEY = 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'
 
 
