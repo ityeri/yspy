09b8870d082d326b870cc05076e6ca889177e922
ityeri <ityeri9@gmail.com>
2026-05-23 20:09:31 +0900

    refactor: Typo VideNextPage -> VideoNextPage

--- src/yspy/page_parsing/video_next_page.py
+++ src/yspy/page_parsing/video_next_page.py
@@ -8,13 +8,13 @@ from .exceptions import PageParsingException
 
 
 @dataclass
-class VideNextPage:
+class VideoNextPage:
     comment_continuation_token: str
 
     @staticmethod
-    def from_json(raw_data: dict[str, dict]) -> VideNextPage:
+    def from_json(raw_data: dict[str, dict]) -> VideoNextPage:
         try:
-            return VideNextPage(
+            return VideoNextPage(
                 comment_continuation_token=get_by_path(
                     raw_data,
                     'engagementPanels', 0, 'engagementPanelSectionListRenderer content sectionListRenderer',
