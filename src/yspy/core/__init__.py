44132274f55ea5f31f1ff914b7b8dc9bccb75a9d
ityeri <ityeri9@gmail.com>
2026-09-07 07:49:06 +0000

    rm: Remove legacy modules (search, extras, streamurlfetcher, core, handlers, future) and legacy tests

--- src/yspy/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .video import VideoCore
-from . import constants, core_utils
-
-__all__ = [
-    'VideoCore', 'constants', 'core_utils.py'
-]
--- src/yspy/handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import component_handler, request_handler
-
-__all__ = [
-    'component_handler', 'request_handler'
-]
