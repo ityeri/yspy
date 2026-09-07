cbd541252adcf80e4426afd0b01aeb233961d7ca
ityeri <ityeri9@gmail.com>
2026-09-07 01:43:16 +0000

    edit: Pair search components with eng components by id instead of position

--- src/yspy/api/search.py
+++ src/yspy/api/search.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 from httpx import AsyncClient, Client
 
-from yspy.data_parsing.search_result import SearchResultPage
+from yspy.data_parsing.search_result import SearchResultPage, SearchResultComponent, VideoComponent, ChannelComponent
 from yspy.request import SearchResultRequest
 from yspy.utils import SearchMode, Locale, ENGLISH_LOCALE, Language, Unspecified, NONE_LOCALE
 from .search_result_element import SearchResultElement, VideoResultElement, ChannelResultElement, \
@@ -55,14 +55,26 @@ class Search:
     @staticmethod
     def from_search_result_page(
             search_result_page: SearchResultPage,
-            eng_search_result_page: SearchResultPage,
+            eng_search_result_page: SearchResultPage | None = None,
             search_mode: SearchMode | None = None,
             locale: Locale = NONE_LOCALE
     ) -> Search:
+        # position of components differ between locale and english pages (ad drops, ranking)
+        # so components are paired by their own id (videoId / channel browseId)
+        if eng_search_result_page is None:
+            eng_by_id: dict[str, SearchResultComponent] = {}
+        else:
+            eng_by_id = {
+                Search._component_key(component): component
+                for component in eng_search_result_page.components
+            }
+
         return Search(
             results=[
-                from_search_result_component(component, eng_component, locale)
-                for (component, eng_component) in zip(search_result_page.components, eng_search_result_page.components)
+                from_search_result_component(
+                    component, eng_by_id.get(Search._component_key(component), component), locale
+                )
+                for component in search_result_page.components
             ],
             search_mode=search_mode,
             locale=locale,
@@ -148,3 +160,12 @@ class Search:
             return self.channels[0]
         except IndexError:
             return default
+
+    @staticmethod
+    def _component_key(component: SearchResultComponent) -> str:
+        if isinstance(component, VideoComponent):
+            return f'video:{component.id}'
+        elif isinstance(component, ChannelComponent):
+            return f'channel:{component.id}'
+        else:
+            raise TypeError('Unknown type SearchResultComponent has passed')
